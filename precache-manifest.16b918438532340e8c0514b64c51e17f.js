d9a8b4603f66dad6ee0873170681cc4d0f5705ae
Gvozd <Gvozd@users.noreply.github.com>
2020-03-11 13:43:57 +0000

    deploy: 344031ef8e4ae28b721983f3d0660fdc3975c845

---
+++ precache-manifest.16b918438532340e8c0514b64c51e17f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604d452bf7eee116f917641c80a49316",
+    "url": "/habidoist/index.html"
+  },
+  {
+    "revision": "70c838aed49589058f3d",
+    "url": "/habidoist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e532ea97ccea6bc865ca",
+    "url": "/habidoist/static/js/2.a5dd82a5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/habidoist/static/js/2.a5dd82a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c838aed49589058f3d",
+    "url": "/habidoist/static/js/main.a6561bad.chunk.js"
+  },
+  {
+    "revision": "064e975ece1072431cbc",
+    "url": "/habidoist/static/js/runtime-main.e41abc24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/habidoist/static/media/logo.5d5d9eef.svg"
+  }
+]);
